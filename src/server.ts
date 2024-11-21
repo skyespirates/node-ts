ecf3db191f2430a6919ec89601c95e4269d902ae
Aziz <akashiseijurou414@gmail.com>
2024-11-21 07:03:59 +0700

    add logging mechanism on request and response

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,43 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from "express";
+import logger from "./utils/logger";
 
 const app = express();
 const port = 3000;
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(`${req.method} ${req.url}`);
+  res.on("finish", () => {
+    logger.info(`${req.method} ${res.statusCode} ${req.url}`);
+  });
+  next();
+});
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   const data = {
-    message: 'Hello, world!',
+    message: "Hello, world!",
   };
   res.json(data);
 });
 
-app.get('/api/items', (req: Request, res: Response) => {
+app.get("/api/items", (req: Request, res: Response) => {
   const data = [
-    { id: 1, name: 'item1' },
-    { id: 2, name: 'item2' },
-    { id: 3, name: 'item3' },
+    { id: 1, name: "item1" },
+    { id: 2, name: "item2" },
+    { id: 3, name: "item3" },
   ];
   res.json(data);
 });
 
-app.post('/api/items', (req: Request, res: Response) => {
+app.post("/api/items", (req: Request, res: Response) => {
   const newItem = req.body.item;
-  res.status(201).json({ message: 'Item created', item: newItem });
+  res.status(201).json({ message: "Item created", item: newItem });
+});
+
+// Middleware to log errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  res.status(500).send("Something went wrong!");
 });
 
 app.listen(port, () => {
