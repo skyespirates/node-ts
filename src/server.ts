4f84cb159c11e6524127188d2b2227b23746fa02
Aziz <aziz_inc@apps.ipb.ac.id>
2024-11-23 21:31:05 +0700

    resolve merged issue

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import logger from "./utils/logger";
+import jwt from 'jsonwebtoken';
 
 const app = express();
 const port = 3000;
@@ -13,14 +14,46 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+const secretKey = 'your_secret_key';
+
+// Middleware to verify JWT
+const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (token) {
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// Login route to generate JWT
+app.post('/login', (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  // This is a mock user authentication
+  if (username === 'user' && password === 'password') {
+    const user = { username };
+    const token = jwt.sign(user, secretKey, { expiresIn: '1h' });
+    res.json({ token });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.get('/', (req: Request, res: Response) => {
   const data = {
     message: "Hello, world!",
   };
   res.json(data);
 });
 
-app.get("/api/items", (req: Request, res: Response) => {
+// Protect the /api/items route with JWT authentication
+app.get('/api/items', authenticateJWT, (req: Request, res: Response) => {
   const data = [
     { id: 1, name: "item1" },
     { id: 2, name: "item2" },
@@ -29,7 +62,8 @@ app.get("/api/items", (req: Request, res: Response) => {
   res.json(data);
 });
 
-app.post("/api/items", (req: Request, res: Response) => {
+// Protect the /api/items POST route with JWT authentication
+app.post('/api/items', authenticateJWT, (req: Request, res: Response) => {
   const newItem = req.body.item;
   res.status(201).json({ message: "Item created", item: newItem });
 });
@@ -43,3 +77,11 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
