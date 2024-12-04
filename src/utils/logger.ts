f33e6264915a8af019a68351cc259df652f81dd7
Aziz <aziz_inc@apps.ipb.ac.id>
2024-12-04 21:51:06 +0700

    add colorize levels log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,29 @@
-import winston from "winston";
+import winston from 'winston';
 
+const myCustomLevels = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+  },
+  colors: {
+    error: 'red',
+    warn: 'yellow',
+    info: 'green',
+  },
+};
+winston.addColors(myCustomLevels.colors);
 const logger = winston.createLogger({
-  level: "info",
+  levels: myCustomLevels.levels,
   format: winston.format.combine(
+    winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.json(),
+    winston.format.simple()
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
